a83f9be0d79c337a9821c163642a57f4fcc2972b
Guillermo Gonzalez <guillermo.gonzalez@me.com>
2013-08-27 16:16:10 +0200

    Add book model

---
+++ ReadingList/TGRBook.h
@@ -0,0 +1,19 @@
+//
+//  TGRBook.h
+//  ReadingList
+//
+//  Created by guille on 27/08/13.
+//  Copyright (c) 2013 Guillermo Gonzalez. All rights reserved.
+//
+
+@interface TGRBook : MTLModel <MTLJSONSerializing>
+
+@property (copy, nonatomic, readonly) NSString *author;
+@property (copy, nonatomic, readonly) NSString *overview;
+@property (copy, nonatomic, readonly) NSArray *genres;
+@property (copy, nonatomic, readonly) NSDate *releaseDate;
+@property (copy, nonatomic, readonly) NSNumber *identifier;
+@property (copy, nonatomic, readonly) NSString *title;
+@property (copy, nonatomic, readonly) NSURL *coverURL;
+
+@end
